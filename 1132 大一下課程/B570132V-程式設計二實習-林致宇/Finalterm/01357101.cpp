c2f1934882ab893aa2bd6b2ee5271cf68bcde1e0
whitebear13579 <huangpeter0927@gmail.com>
2025-06-20 01:49:49 +0800

    fix: header file declaration

--- 1132 大一下課程/B570132V-程式設計二實習-林致宇/Finalterm/01357101.cpp
+++ 1132 大一下課程/B570132V-程式設計二實習-林致宇/Finalterm/01357101.cpp
@@ -1,4 +1,4 @@
--#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #define whitebear ios_base::sync_with_stdio(0) , cin.tie(0) , cout.tie(0)
 #define vi vector<int>
 #define vii vector<vector<int>>
